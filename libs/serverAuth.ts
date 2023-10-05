dbfc9eff9e31395aa3d6e8e2ef063c47084079cc
Asad Ullah <pixaque@gmail.com>
2023-10-05 14:35:48 +0500

    fixed the session issue

--- libs/serverAuth.ts
+++ libs/serverAuth.ts
@@ -3,26 +3,48 @@ import { getSession } from "next-auth/react";
 
 import prismadb from '@/libs/prismadb';
 
+import { getToken } from "next-auth/jwt";
+
+const secret = process.env.NEXTAUTH_SECRET;
+
+
 const serverAuth = async (req: NextApiRequest) => {
-  const session = await getSession({ req });
-  
-  if (!session?.user?.email) {
-    throw new Error('Not signed in');
-  }
-  
-  const currentUser = await prismadb.user.findUnique({
-    where: {
-      email: session?.user?.email,
+
+    const token = await getToken({ req, secret })
+    console.log("JSON Web Token", token)
+
+    //const session = await getSession({ req });
+   // console.log("Session: ", session);
+
+    if (!token?.email) {
+      throw new Error('Not signed in');
     }
-  });
+    
+    const currentUser = await prismadb.user.findUnique({
+      where: {
+        email: token?.email,
+      }
+    });
+    
+    if (!currentUser) {
+      throw new Error('Not signed in');
+    }
   
-  if (!currentUser) {
-    throw new Error('Not signed in');
-  }
-
-  console.log(currentUser)
-
-  return { currentUser };
+    console.log(currentUser)
+  
+    return { currentUser };
+  
+  /*
+  const session = {
+    user: {
+      name: 'pixaque',
+      email: 'pixaque@gmail.com',
+      image: 'https://avatars.githubusercontent.com/u/5223617?v=4'
+    },
+    expires: '2023-11-03T03:31:16.636Z'
+  };
+  */
+  
 }
 
 export default serverAuth;
