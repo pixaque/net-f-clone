928e9c0689a76e5c19f8911c7f6ff9edc608cf3e
Asad Ullah <pixaque@gmail.com>
2023-10-12 05:35:24 +0500

    navbar update...

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,12 @@ import { BellIcon, MagnifyingGlassIcon, ChevronDownIcon } from '@heroicons/react
 import AccountMenu from '@/components/AccountMenu';
 import MobileMenu from '@/components/MobileMenu';
 import NavbarItem from '@/components/NavbarItem';
+import useCurrentUser from '@/hooks/useCurrentUser';
 
 const TOP_OFFSET = 66;
 
 const Navbar = () => {
+  const { data: currentUser } = useCurrentUser();
   const [showAccountMenu, setShowAccountMenu] = useState(false);
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [showBackground, setShowBackground] = useState(false);
@@ -42,12 +44,12 @@ const Navbar = () => {
       <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
         <img src="/images/logo.png" className="h-4 lg:h-7" alt="Logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
-          <NavbarItem label="Home" active />
-          <NavbarItem label="Series" />
-          <NavbarItem label="Films" />
-          <NavbarItem label="New & Popular" />
-          <NavbarItem label="My List" />
-          <NavbarItem label="Browse by Languages" />
+          <NavbarItem label="Home" linkPage="/" />
+          <NavbarItem label="Series" linkPage="/" />
+          <NavbarItem label="Films" linkPage="/" />
+          <NavbarItem label="New & Popular" linkPage="/newpopular" />
+          <NavbarItem  label="My List" linkPage="/my_list" />
+          <NavbarItem label="Browse by Languages" linkPage="/" />
         </div>
         <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
           <p className="text-white text-sm">Browse</p>
@@ -63,7 +65,7 @@ const Navbar = () => {
           </div>
           <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
             <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
-              <img src="/images/default-blue.png" alt="" />
+              <img src={currentUser?.image || '/images/default-green.png'} alt={currentUser?.name} />
             </div>
             <ChevronDownIcon className={`w-4 text-white fill-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
             <AccountMenu visible={showAccountMenu} />
--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import { useRouter } from "next/router";
+import react, { useCallback, useState } from "react";
 
 interface NavbarItemProps {
   label: string;
   active?: boolean;
+  linkPage: string;
 }
 
-const NavbarItem: React.FC<NavbarItemProps> = ({ label, active }) => {
+const NavbarItem: React.FC<NavbarItemProps> = ({ label, linkPage, active }) => {
+
+  const [activePage, setActivePage] = useState(false);
+  const router = useRouter();
+
+  const selectPage = useCallback(() => {
+
+    //setActivePage(true);
+    //alert(activePage);
+    console.log("Active: ", activePage);
+    router.push(linkPage);
+    console.log("Link: ", linkPage);
+    
+
+    handleClick();
+    
+
+  }, [router]);
+
+  const handleClick = () => {
+    
+    setTimeout(() => {
+
+      setActivePage(true);
+      console.log("Active click: ", activePage);
+
+    }, 1000);
+
+  };
+
   return (
-    <div className={active ? 'text-white cursor-default' : 'text-gray-200 hover:text-gray-300 cursor-pointer transition'}>
+    <div onClick={() => selectPage()} className={activePage === true ? 'text-white font-bold cursor-default' : 'text-gray-200 hover:text-gray-300 cursor-pointer transition'}>
       {label}
     </div>
   )
