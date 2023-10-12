5e65596433d321c3f7566fe6d542e2a9f02b8158
Asad Ullah <pixaque@gmail.com>
2023-10-12 06:24:04 +0500

    Added Privacy Policy page.

---
+++ pages/privacy.tsx
@@ -0,0 +1,17 @@
+import Navbar from '@/components/Navbar';
+
+const PrivacyPolicy = () => {
+
+    return (
+      <>
+      <Navbar />
+      
+      <div className="pt-20 pb-40">
+        <h1>Privacy Policy</h1>
+        <p>This is my privacy policy page.</p>
+      </div>
+    </>
+    );
+  }
+  
+  export default PrivacyPolicy;
